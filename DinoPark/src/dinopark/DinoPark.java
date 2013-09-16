e90bad2763728a56c73c8150bf4a62d590adb18f
Nick Carlough <NickCarlough@Nicks-MacBook-Pro-2.local>
2013-09-16 18:37:04 -0500

    First Commit

---
+++ DinoPark/src/dinopark/DinoPark.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dinopark;
+
+/**
+ *
+ * @author NickCarlough
+ */
+public class DinoPark {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
